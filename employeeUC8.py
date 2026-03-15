dff21d024719f774273dea320e6065f14c1f82bc
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-15 12:43:52 +0530

    feat(employee-wage): store daily employee wages in list during monthly wage computation

---
+++ employeeUC8.py
@@ -0,0 +1,40 @@
+import random
+
+# Constants
+EMP_ABSENT = 0
+EMP_FULL_TIME = 1
+EMP_PART_TIME = 2
+
+WAGE_PER_HOUR = 20
+FULL_DAY_HOURS = 8
+PART_TIME_HOURS = 4
+
+MAX_WORKING_DAYS = 20
+MAX_WORKING_HOURS = 100
+
+total_emp_hours = 0
+total_working_days = 0
+daily_wage_list = []
+
+while total_emp_hours <= MAX_WORKING_HOURS and total_working_days < MAX_WORKING_DAYS:
+
+    total_working_days += 1
+
+    emp_check = random.randint(0,2)
+
+    if emp_check == EMP_FULL_TIME:
+        emp_hours = FULL_DAY_HOURS
+    elif emp_check == EMP_PART_TIME:
+        emp_hours = PART_TIME_HOURS
+    else:
+        emp_hours = 0
+
+    total_emp_hours += emp_hours
+
+    daily_wage = emp_hours * WAGE_PER_HOUR
+    daily_wage_list.append(daily_wage)
+
+total_wage = total_emp_hours * WAGE_PER_HOUR
+
+print("Daily Wages:", daily_wage_list)
+print("Total Wage:", total_wage)
